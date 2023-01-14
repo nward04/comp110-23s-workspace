8ed4ab55b89ddf98190a578bf48ed2453b48875f
Nathaniel Ward <nward@ad.unc.edu>
2023-01-13 21:12:13 -0500

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first program for COMP110."""
+
+__author__ = "730553832"
+
+print("Hello, world.")
+print("Good morning starshine, the world says Hello")
+print("Hellos worlds")
+#Finished Exercies 00! I am not sure how to access the message box so I used this comment.
